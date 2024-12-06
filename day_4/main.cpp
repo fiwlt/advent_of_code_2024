cf26c68ab8787df741a4c0e8bd93bdc9ab11433a
Martin <26838196+fiwlt@users.noreply.github.com>
2024-12-06 02:28:13 +0100

    reduce allocs

--- day_4/main.cpp
+++ day_4/main.cpp
@@ -18,11 +18,13 @@ struct word_inst {
   std::vector<direction> dir{};
 };
 
-auto update_position(std::vector<word_inst> &set, const char &c,
-                          int64_t &count,
-                          std::vector<std::vector<word_inst>> &c_line,
-                          const size_t &line_idx, direction dir,
-                          std::map<char, std::string_view> &word_list) -> void {
+const std::map<char, std::string_view> word_list = {{'X', "XMAS"},
+                                                    {'S', "SAMX"}};
+
+auto update_position(std::vector<word_inst> &set, const char &c, int64_t &count,
+                     std::vector<std::vector<word_inst>> &c_line,
+                     const size_t &line_idx, direction dir) -> void {
+//                     std::map<char, std::string_view> &word_list) -> void {
   for (auto &w : set) {
     std::string_view s{word_list.at(w.word_key)};
     if (c == s.at(w.char_pos + 1)) {
@@ -62,18 +64,17 @@ int main(const int argc, const char *argv[]) {
   std::string buf{};
   std::vector<int64_t> results{0, 0};
 
-  std::map<char, std::string_view> word_list;
-  word_list['X'] = "XMAS";
-  word_list['S'] = "SAMX";
   std::vector<std::vector<word_inst>> p_line{};
   std::vector<std::vector<word_inst>> c_line{};
+  std::vector<word_inst> p_set{};
+  std::vector<word_inst> c_set{};
 
   while (std::getline(file, buf)) {
     for (const auto &c : buf) {
-      std::vector<word_inst> c_set{};
       if (!c_line.empty() && !c_line.back().empty()) {
-        std::copy(c_line.back().begin(), c_line.back().end(),
-                  std::back_inserter(c_set));
+        c_set = c_line.back();
+      } else {
+        c_set = {};
       }
       c_line.push_back({});
       if (word_list.contains(c)) {
@@ -83,25 +84,19 @@ int main(const int argc, const char *argv[]) {
       }
 
       size_t line_idx{c_line.size() - 1};
-      update_position(c_set, c, results.at(0), c_line, line_idx, right,
-                           word_list);
+      update_position(c_set, c, results.at(0), c_line, line_idx, right);
 
       // check previous line
       if (!p_line.empty()) {
-        std::vector<word_inst> p_set{};
         for (auto &offset : {-1, 0, 1}) {
-          p_set.clear();
           if (line_idx + offset >= 0 && line_idx + offset < p_line.size()) {
-            std::copy(p_line.at(line_idx + offset).begin(),
-                      p_line.at(line_idx + offset).end(),
-                      std::back_inserter(p_set));
+            p_set = p_line.at(line_idx + offset);
             update_position(p_set, c, results.at(0), c_line, line_idx,
-                                 direction(offset), word_list);
+                            direction(offset));
           }
         }
       }
     }
-
     p_line.swap(c_line);
     c_line.clear();
   }
