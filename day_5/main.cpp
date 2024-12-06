6093f68d2f3d75340c8df935711ed51756f68465
Martin <26838196+fiwlt@users.noreply.github.com>
2024-12-06 20:18:36 +0100

    day 5, part 2 complete

--- day_5/main.cpp
+++ day_5/main.cpp
@@ -1,6 +1,7 @@
 // result part 1: 3608
-// result part 2:
+// result part 2: 4922
 
+#include <algorithm>
 #include <cassert>
 #include <charconv>
 #include <deque>
@@ -10,7 +11,19 @@
 #include <map>
 #include <ranges>
 #include <vector>
-#include <algorithm>
+
+struct page {
+  uint32_t v{};
+  std::vector<uint32_t> pre{};
+  bool operator<(const page &other) {
+    for (const auto &p : pre) {
+      if (other.v == p) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 
 auto valid_update(const std::string &buf,
                   std::map<uint32_t, std::vector<uint32_t>> &f_pmap,
@@ -27,40 +40,64 @@ auto valid_update(const std::string &buf,
     std::from_chars(s.data(), s.data() + s.size(), u);
     p.push_back(u);
   }
-  for (const auto &v: p){
-  std::cout << v << " ";
-  }
-  std::cout << std::endl;
   std::map<uint32_t, std::vector<uint32_t>> t_f_pmap{};
   std::map<uint32_t, std::vector<uint32_t>> t_r_pmap{};
 
-  //tailor maps
-  for(const auto &i: p){
-    for(const auto &v: f_pmap[i]){
-      if (std::find(p.begin(), p.end(), v) != p.end()){
+  // tailor maps
+  for (const auto &i : p) {
+    for (const auto &v : f_pmap[i]) {
+      if (std::find(p.begin(), p.end(), v) != p.end()) {
         t_f_pmap[i].push_back(v);
       }
     }
-    for(const auto &v: r_pmap[i]){
-      if (std::find(p.begin(), p.end(), v) != p.end()){
+    for (const auto &v : r_pmap[i]) {
+      if (std::find(p.begin(), p.end(), v) != p.end()) {
         t_r_pmap[i].push_back(v);
       }
     }
   }
 
   bool is_valid{true};
-  for(const auto &i: p){
-    std::cout << t_f_pmap[i].empty() << " ";
-    if (!t_f_pmap[i].empty()){is_valid = false; break;}
-    for (auto &ri: t_r_pmap[i]){
+  for (const auto &i : p) {
+    if (!t_f_pmap[i].empty()) {
+      is_valid = false;
+      break;
+    }
+    for (auto &ri : t_r_pmap[i]) {
       t_f_pmap[ri].pop_back();
     }
   }
-  std::cout << std::endl;
-  if (is_valid){
-    score += p.at((p.size()-1)/2);
+  if (is_valid) {
+    score += p.at((p.size() - 1) / 2);
   }
-  return false;
+
+  return is_valid;
+}
+
+auto make_valid(const std::string &buf,
+                std::map<uint32_t, std::vector<uint32_t>> &f_pmap,
+                std::map<uint32_t, std::vector<uint32_t>> &r_pmap,
+                int64_t &score) -> void {
+  auto tokens =
+      buf | std::views::split(',') | std::views::transform([](auto t) {
+        return std::string_view(t.begin(), t.end());
+      });
+  std::vector<page> pages{};
+  for (const auto &t : tokens) {
+    auto s{std::string_view(t.begin(), t.end())};
+    uint32_t u;
+    std::from_chars(s.data(), s.data() + s.size(), u);
+    page tmp{};
+    tmp.v = u;
+    pages.push_back(tmp);
+  }
+  for (auto &p : pages) {
+    if (f_pmap.contains(p.v)) {
+      p.pre = f_pmap.at(p.v);
+    }
+  }
+  std::sort(pages.begin(), pages.end(), std::less());
+  score += pages.at((pages.size() - 1) / 2).v;
 }
 
 auto parse_rule(const std::string &buf,
@@ -107,7 +144,10 @@ int main(const int argc, const char *argv[]) {
   while (std::getline(file, buf)) {
     if (eval_updates) {
       // evaluate updates
-      valid_update(buf, f_pmap, r_pmap, results.at(0));
+      bool is_valid = valid_update(buf, f_pmap, r_pmap, results.at(0));
+      if (!is_valid) {
+        make_valid(buf, f_pmap, r_pmap, results.at(1));
+      }
     } else {
       // read page pairs
       if (buf.empty()) {
